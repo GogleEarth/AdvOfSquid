b479b48c3c13e458c6bc7c6f886e2c8dba0dede4
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2020-03-10 16:49:03 +0900

    200310
    공중점프, 대시 수정

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,18 +7,25 @@ public class PlayerController : MonoBehaviour
     public float MOVE_FORCE = 1f;
     public float COF; // 마찰계수
     public float MAX_SPEED = 10f;
-    public float JIMP_FORCE = 10f;
-    public int MAX_AIR_DASH_COUNT = 1;
-
+    public float JUMP_FORCE = 10f;
+    public float DASH_COOLTIME = 0.5f;
+    public float DASH_FORCE = 5f;
+    public int MAX_AIR_DASH_CNT = 1;
+    public int MAX_AIR_JUMP_CNT = 0;
+    
     bool move_left;
     bool move_right;
     bool onground;
     bool down_key;
+    bool jump_key;
     Vector2 dir;
     float curr_speed;
     float force;
     int air_dash_count;
+    int air_jump_count;
     Rigidbody2D squid_rigidbody;
+    float jump_force;
+    float dash_cooltime;
 
     void Start()
     {
@@ -26,16 +33,22 @@ public class PlayerController : MonoBehaviour
         move_right = false;
         onground = false;
         down_key = false;
+        jump_key = false;
         dir.x = 0f;
         dir.y = 0f;
         force = 0f;
         curr_speed = 0f;
         air_dash_count = 0;
+        air_jump_count = 0;
+        jump_force = 0f;
+        dash_cooltime = DASH_COOLTIME;
         squid_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        if (dash_cooltime > 0f) dash_cooltime -= Time.deltaTime;
+
         ProcessInput();
 
         if (move_left && move_right) dir.x = 0f;
@@ -51,6 +64,12 @@ public class PlayerController : MonoBehaviour
             force += MOVE_FORCE;
         else
             force = 0f;
+
+        if (jump_key)
+        {
+            if (jump_force < JUMP_FORCE) jump_force += 1f;
+            else jump_force = JUMP_FORCE;
+        }
 
         float prev_speed = curr_speed;
         if (onground) curr_speed += (force - (2 * COF * 9.8f)) * Time.deltaTime; 
@@ -96,8 +115,14 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (!onground)
+            if (!down_key)
                 down_key = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (!jump_key && onground)
+                jump_key = true;
         }
 
         if (Input.GetKeyUp(KeyCode.LeftArrow))
@@ -115,36 +140,69 @@ public class PlayerController : MonoBehaviour
             down_key = false;
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.X))
         {
             Jump();
+            jump_key = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            Dash();
         }
     }
-
+    
     private void Jump()
     {
         if (onground)
         {
-            squid_rigidbody.AddForce(Vector2.up * JIMP_FORCE, ForceMode2D.Impulse);
+            squid_rigidbody.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
+            jump_force = 0f;
         }
         else if (down_key)
         {
-            squid_rigidbody.AddForce(Vector2.up * 3 * -JIMP_FORCE, ForceMode2D.Impulse);
+            squid_rigidbody.AddForce(Vector2.up * 3 * -JUMP_FORCE, ForceMode2D.Impulse);
         }
-        else if (move_left)
+        else
         {
-            if (air_dash_count < MAX_AIR_DASH_COUNT)
+            if (air_jump_count < MAX_AIR_JUMP_CNT)
             {
-                squid_rigidbody.AddForce(Vector2.right * -JIMP_FORCE, ForceMode2D.Impulse);
-                air_dash_count++;
+                squid_rigidbody.AddForce(Vector2.up * JUMP_FORCE, ForceMode2D.Impulse);
+                air_jump_count++;
             }
         }
-        else if (move_right)
+    }
+
+    private void Dash()
+    {
+        if (!onground)
         {
-            if (air_dash_count < MAX_AIR_DASH_COUNT)
+            if (move_left)
             {
-                squid_rigidbody.AddForce(Vector2.right * JIMP_FORCE, ForceMode2D.Impulse);
-                air_dash_count++;
+                if (air_dash_count < MAX_AIR_DASH_CNT)
+                {
+                    squid_rigidbody.AddForce(Vector2.right * -DASH_FORCE, ForceMode2D.Impulse);
+                    air_dash_count++;
+                }
+            }
+            else if (move_right)
+            {
+                if (air_dash_count < MAX_AIR_DASH_CNT)
+                {
+                    squid_rigidbody.AddForce(Vector2.right * DASH_FORCE, ForceMode2D.Impulse);
+                    air_dash_count++;
+                }
+            }
+        }
+        else
+        {
+            if (dash_cooltime <= 0f)
+            {
+                if (move_left)
+                    squid_rigidbody.AddForce(Vector2.right * -DASH_FORCE, ForceMode2D.Impulse);
+                else if (move_right)
+                    squid_rigidbody.AddForce(Vector2.right * DASH_FORCE, ForceMode2D.Impulse);
+                dash_cooltime = DASH_COOLTIME;
             }
         }
     }
@@ -155,6 +213,7 @@ public class PlayerController : MonoBehaviour
         {
             onground = true;
             air_dash_count = 0;
+            air_jump_count = 0;
         }
     }
 
